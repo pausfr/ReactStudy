fb758865cff64382f50f96a02c42d0daa3c74d39
pausfr <lost7l@icloud.com>
2022-01-10 10:46:51 +0900

    09 추가

---
+++ ReactWebGame/09/client.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import TicTacToe from "./TicTacToe"; // 함수형 컴포넌트 사용
+
+ReactDOM.render(<TicTacToe />, document.querySelector("#root"));
