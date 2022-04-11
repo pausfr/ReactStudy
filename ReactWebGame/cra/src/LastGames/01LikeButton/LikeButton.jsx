196d900dc3c8c89ad46801f40ba8e7913b8f2540
pausfr <lost7l@icloud.com>
2022-04-11 14:01:08 +0900

    btn test

--- ReactWebGame/cra/src/LastGames/01LikeButton/LikeButton.jsx
+++ ReactWebGame/cra/src/LastGames/01LikeButton/LikeButton.jsx
@@ -1,8 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 
 export default function LikeButton({ nickname }) {
   const [liked, setLiked] = useState(false);
+  let cnt = useRef(0);
+  const [tmp, setTmp] = useState(false);
+
+  const onClickTmp = () => {
+    console.log(cnt);
+    if (cnt.current % 3 === 0) {
+      setTmp(true);
+      cnt.current++;
+    } else {
+      setTmp(false);
+      cnt.current++;
+    }
+  };
 
   return (
     <>
@@ -16,6 +29,8 @@ export default function LikeButton({ nickname }) {
         >
           {liked ? "Liked" : "Like"}
         </button>
+
+        <button onClick={onClickTmp}>{tmp ? "True" : "False"}</button>
       </div>
     </>
   );
